eb99e54ff7b79e384555ec54048303184b2488e2
Mi Yeon Peach <peachkat@uw.edu>
2019-07-31 17:26:31 -0700

    deploy push

---
+++ docs/build/precache-manifest.83e2477a6266ac934a56b82dca25c824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b08a569a70f84ff33b9ced910b6bed",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f959ce416d0046fb9383",
+    "url": "/portfolio/static/css/main.0cf62bc1.chunk.css"
+  },
+  {
+    "revision": "f5dbdcab61336c2c7c2e",
+    "url": "/portfolio/static/js/2.70719442.chunk.js"
+  },
+  {
+    "revision": "f959ce416d0046fb9383",
+    "url": "/portfolio/static/js/main.72f49a7b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4317be933b8b615a4b7847d40093ae3f",
+    "url": "/portfolio/static/media/header.4317be93.jpg"
+  }
+]);
